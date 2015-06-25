7e975e4f4ea77e8808d3bab2385f5b72479d41a7
Sergey Samoylov <ssamoylov@justcoded.com>
2015-06-25 15:21:11 +0300

    Add a posibility to remove and add rows to table and save data of tables, update setting page for table fields

--- components/table/table.js
+++ components/table/table.js
@@ -1,18 +1,24 @@
 var add_row_btn = '.jcf_add_row';
-function addRowToTable(node) {
-	var count_cols = node.find( 'td' ).lenght();
-	console.log(count_cols);
-	node.append()
-}
-
+var delete_row_btn = '.jcf_delete_row';
 jQuery(document).ready(function() {
+	
 	jQuery( add_row_btn ).click(function() {
-		var tbl = jQuery( this ).parent().parent().find('table');
-		var new_row = tbl.find('tr:last-child').clone();
-		tbl.append(new_row);
+		var container = jQuery( this ).parent().parent().find('table');
+		var jcf_table_row_max_index = container .find('tr').size();
+		jcf_table_row_max_index--;
+		var new_row = container.find('tr:last-child').html();
+		new_row = new_row.replace(/\[00\]/g, '[' + jcf_table_row_max_index + ']')
+			.replace(/\-00\-/g, '-' + jcf_table_row_max_index + '-');
+		new_row = '<tr>' + new_row + '</tr>';
+		container.find('tr:last-child').before(new_row);
 		return false;
 	});
+	
+	jQuery( delete_row_btn ).live('click', function() {
+		jQuery( this ).parent().parent().remove();
+	})
+	
 	jQuery( '.sortable' ).each(function() {
-		jQuery( this ).find( 'tbody' ).sortable({ containment: jQuery( this ), scroll: false });
+		jQuery( this ).find( 'tbody' ).sortable({ containment: jQuery( this ), scroll: false, items: 'tr[class!=table-header]' });
 	});
 });
--- components/table/table.php
+++ components/table/table.php
@@ -16,44 +16,114 @@ class Just_Field_Table extends Just_Field{
 	 *	draw field on post edit form
 	 *	you can use $this->instance, $this->entry
 	 */
+	
 	function field( $args ) {
 		extract( $args );
 		
 		echo $before_widget;
 		echo $before_title . $this->instance['title'] . $after_title;
+		
+		if(empty($this->entry)) $this->entry = array('0' => '');
+		// add null element for etalon copy
+		$entries = (array)$this->entry;
+
+		// get fields
+		$_col_names = explode("\n", $this->instance['col_names']);
+		foreach($_col_names as $line){
+			$line = trim($line);
+			$col_name = explode('|', $line);
+			if( count($col_name) == 2 ){
+				$col_names[ $col_name[0] ] = $col_name[1];
+			}
+		}
+		
+		if( empty($col_names) ){
+			echo '<p>'.__('Wrong col names configuration. Please check widget settings.', JCF_TEXTDOMAIN).'</p>';
+		}
+		
+		$count_cols = count($col_names);
+		$first_td = array();
+		
+		if( !empty($col_names) ) :
 		?>
-		<table class="sortable">
-		<?php for( $i = 0; $i <= $this->instance['rows']; $i++ ): ?>
-			<tr>
-			<?php for( $j = 0; $j <= $this->instance['cols']; $j++ ): ?>
-					<?php if( $i == 0 ): ?>
-						<?php if( $j == 0 ): ?>
-							<th><span class="drag-handle" >move</span></th>
+		<div class="jcf-table jcf-field-container">
+			<table class="sortable">
+				<?php foreach($entries as $key => $entry) : ?>
+					<tr <?php echo $key == 0 ? 'class="table-header"' : ''; ?>>
+						<?php if( $key == 0 ): ?>
+							<th>Options</th>
 						<?php else : ?>
-							<th><input type="text" name="<?php echo $this->get_field_name('val');?>" id="<?php echo $this->get_field_id('val'); ?>" value="" placeholder="column name"/></th>
-						<?php endif ; ?>
-					<?php else: ?>
-						<?php if( $j == 0 ): ?>
-							<td><span class="drag-handle" >move</span></td>
-						<?php else : ?>
-							<td><input type="text" name="<?php echo $this->get_field_name('val');?>" id="<?php echo $this->get_field_id('val'); ?>" value=""/></td>
-						<?php endif ; ?>
-						
-					<?php endif; ?>
-			<?php endfor; ?>
-			</tr>
-		<?php endfor; ?>
-		</table>
-		<p><a href="#" class="jcf-btn jcf_add_row"><?php _e('+ Add row', JCF_TEXTDOMAIN); ?></a></p>
+							<td>
+								<span class="drag-handle" >move</span>
+								<span class="jcf_delete_row" >delete</span>
+							</td>
+						<?php endif; ?>
+						<?php foreach($col_names as $col_name => $col_title) : 
+							if( !empty($entry[$col_name]) ) $col_title = esc_attr(@$entry[$col_name]);
+						?>
+							<?php if( $key == 0 ): ?>
+								<th><input type="text" value="<?php echo $col_title; ?>" 
+									id="<?php echo $this->get_field_id_l2($col_name, $key); ?>" 
+									name="<?php echo $this->get_field_name_l2($col_name, $key); ?> ">
+								</th>
+								<?php $first_td[] = '<td><input type="text" value="" 
+										id="' . $this->get_field_id_l2($col_name, '00') . '" 
+										name="' . $this->get_field_name_l2($col_name, '00') . '"></td>';
+								?>
+							<?php else : ?>
+								<td><input type="text" value="<?php echo $col_title; ?>" 
+									id="<?php echo $this->get_field_id_l2($col_name, $key); ?>" 
+									name="<?php echo $this->get_field_name_l2($col_name, $key); ?> ">
+								</td>
+							<?php endif; ?>
+						<?php endforeach; ?>
+					</tr>
+				<?php endforeach; ?>
+				<tr class="hide">
+					<td>
+						<span class="drag-handle" >move</span>
+						<span class="jcf_delete_row" >delete</span>
+					</td>
+					<?php foreach($first_td as $td): ?>
+						<?php echo $td; ?>
+					<?php endforeach; ?>
+				</tr>
+			</table>
+			<p><a href="#" class="jcf-btn jcf_add_row"><?php _e('+ Add row', JCF_TEXTDOMAIN); ?></a></p>
+		</div>
 		<?php
+		endif; 
+
+		if( $this->instance['description'] != '' )
+			echo '<p class="description">' . $this->instance['description'] . '</p>';
+		
 		echo $after_widget;
+		
+		return true;
 	}
 	
 	/**
 	 *	save field on post edit form
 	 */
-	function save( $values ){
-		$values = $values['val'];
+	function save( $_values ){
+		$values = array();
+		if(empty($_values)) return $values;
+	
+		// remove etalon element
+		if(isset($_values['00'])) 
+			unset($_values['00']);
+		
+		// fill values
+		foreach($_values as $key => $params){
+			if(!is_array($params) || !empty($params['__delete__'])){
+				continue;
+			}
+			
+			unset($params['__delete__']);
+			$values[$key] = $params;
+		}
+		
+		$values = array_values($values);
 		return $values;
 	}
 	
@@ -62,29 +132,32 @@ class Just_Field_Table extends Just_Field{
 	 */
 	function update( $new_instance, $old_instance ) {
 		$instance = $old_instance;
+		
 		$instance['title'] = strip_tags($new_instance['title']);
-		$instance['cols'] = strip_tags($new_instance['cols']);
-		$instance['rows'] = strip_tags($new_instance['rows']);
+		$instance['col_names'] = strip_tags($new_instance['col_names']);
 		$instance['description'] = strip_tags($new_instance['description']);
+		
 		return $instance;
 	}
 
 	/**
 	 *	print settings form for field
-	 */	
+	 */		
 	function form( $instance ) {
 		//Defaults
-		$instance = wp_parse_args( (array) $instance, array( 'title' => '' ) );
+		$instance = wp_parse_args( (array) $instance, array( 'title' => '', 'col_names' => '', 'description' => '' ) );
 
 		$title = esc_attr( $instance['title'] );
-		$cols = esc_attr( $instance['cols'] );
-		$rows = esc_attr( $instance['rows'] );
+		$col_names = esc_html( $instance['col_names'] );
 		$description = esc_html($instance['description']);
 		?>
-		<p><label for="<?php echo $this->get_field_id('title'); ?>"><?php _e('Title:', JCF_TEXTDOMAIN); ?></label> <input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo $title; ?>" /></p>
-		<p><label for="<?php echo $this->get_field_id('cols'); ?>"><?php _e('Cols:', JCF_TEXTDOMAIN); ?></label> <input class="widefat" id="<?php echo $this->get_field_id('cols'); ?>" name="<?php echo $this->get_field_name('cols'); ?>" type="text" value="<?php echo $cols; ?>" /></p>
-		<p><label for="<?php echo $this->get_field_id('rows'); ?>"><?php _e('Rows:', JCF_TEXTDOMAIN); ?></label> <input class="widefat" id="<?php echo $this->get_field_id('rows'); ?>" name="<?php echo $this->get_field_name('rows'); ?>" type="text" value="<?php echo $rows; ?>" /></p>
-		<p><label for="<?php echo $this->get_field_id('description'); ?>"><?php _e('Description:', JCF_TEXTDOMAIN); ?></label> <textarea name="<?php echo $this->get_field_name('description'); ?>" id="<?php echo $this->get_field_id('description'); ?>" cols="20" rows="4" class="widefat"><?php echo $description; ?></textarea></p>
+		<p><label for="<?php echo $this->get_field_id('title'); ?>"><?php _e('Title:', JCF_TEXTDOMAIN); ?></label>
+			<input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo $title; ?>" /></p>
+		<p><label for="<?php echo $this->get_field_id('fields'); ?>"><?php _e('Col names:', JCF_TEXTDOMAIN); ?></label> 
+			<textarea name="<?php echo $this->get_field_name('col_names'); ?>" id="<?php echo $this->get_field_id('col_names'); ?>" cols="20" rows="4" class="widefat"><?php echo $col_names; ?></textarea>
+			<br/><small><?php _e('Format: %colname|%coltitle<br/><i>Example: username|User name', JCF_TEXTDOMAIN); ?></i></small></p>
+		<p><label for="<?php echo $this->get_field_id('description'); ?>"><?php _e('Description:', JCF_TEXTDOMAIN); ?></label> 
+			<textarea name="<?php echo $this->get_field_name('description'); ?>" id="<?php echo $this->get_field_id('description'); ?>" cols="20" rows="2" class="widefat"><?php echo $description; ?></textarea></p>
 		<?php
 	}
 	
