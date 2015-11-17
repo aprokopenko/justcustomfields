a309bea8dd3452e300d749192fd184f9aa53999e
alex_prokopenko <ab.prokopenko@gmail.com>
2015-11-17 16:25:15 +0200

    Fixed version number

--- just-custom-fields.php
+++ just-custom-fields.php
@@ -6,13 +6,13 @@ Description: This plugin add custom fields for standard and custom post types in
 Tags: custom, fields, custom fields, meta, post meta, object meta, editor
 Author: Alexander Prokopenko
 Author URI: http://justcoded.com/
-Version: 2.3
+Version: 2.2
 Donate link: http://justcoded.com/just-labs/just-custom-fields-for-wordpress-plugin/
 */
 
 define('JCF_ROOT', dirname(__FILE__));
 define('JCF_TEXTDOMAIN', 'just-custom-fields');
-define('JCF_VERSION', 2.300);
+define('JCF_VERSION', 2.200);
 
 define('JCF_CONF_MS_NETWORK', 'network');
 define('JCF_CONF_MS_SITE', 'site');
