d7c90d53c291c0ac06e5c8ec567213338c842c4a
evkos <evgenykostyuk@mail.ru>
2017-06-15 11:52:25 +0300

    refactor googlemap

--- components/datepicker/JustField_DatePicker.php
+++ components/datepicker/JustField_DatePicker.php
@@ -33,13 +33,13 @@ class JustField_DatePicker extends core\JustField {
 	 */
 	public function field() {
 		?>
-		<div id="jcf_field-<?php echo $this->id; ?>"
-			 class="jcf_edit_field <?php echo $this->field_options['classname']; ?>">
+		<div id="jcf_field-<?php echo esc_attr( $this->id ); ?>"
+			 class="jcf_edit_field <?php echo esc_attr( $this->field_options['classname'] ); ?>">
 			<?php echo $this->field_options['before_widget']; ?>
 			<?php echo $this->field_options['before_title'] . esc_html( $this->instance['title'] ) . $this->field_options['after_title']; ?>
 
 			<div>
-				<input id="<?php echo $this->get_field_id( 'val' ); ?>"
+				<input id="<?php echo esc_attr( $this->get_field_id( 'val' ) ); ?>"
 					   name="<?php echo $this->get_field_name( 'val' ); ?>" type="text"
 					   value="<?php echo esc_attr( $this->entry ); ?>" size="14" style="width:150px;"/>
 				<span class="dashicons dashicons-calendar-alt"></span>
@@ -47,7 +47,7 @@ class JustField_DatePicker extends core\JustField {
 
 			<script type="text/javascript"><!--
 				jQuery(document).ready(function () {
-					jQuery("#<?php echo $this->get_field_id( 'val' ); ?>").datepicker({
+					jQuery("#<?php echo esc_attr( $this->get_field_id( 'val' ) ); ?>").datepicker({
 						dateFormat: "<?php echo ! empty( $this->instance['date_format'] ) ? esc_attr( $this->instance['date_format'] ) : 'yy-mm-dd'; ?>"
 						<?php if ( ! empty( $this->instance['show_monthes'] ) ) {
 						echo ', changeMonth: true, changeYear: true';
@@ -66,10 +66,10 @@ class JustField_DatePicker extends core\JustField {
 	}
 
 	/**
-	 * draw form for edit field
+	 * Draw form for edit field
 	 */
 	public function form() {
-		//Defaults
+		// Defaults.
 		$instance = wp_parse_args( (array) $this->instance, array( 'title' => '' ) );
 
 		$title        = esc_attr( $instance['title'] );
@@ -77,21 +77,23 @@ class JustField_DatePicker extends core\JustField {
 		$date_format  = ! empty( $instance['date_format'] ) ? esc_attr( $instance['date_format'] ) : 'yy-mm-dd';
 		?>
 		<p>
-			<label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Title:', \JustCustomFields::TEXTDOMAIN ); ?></label>
-			<input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>"
-				   name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo $title; ?>"/>
+			<label for="<?php echo esc_attr( $this->get_field_id( 'title' ) ); ?>"><?php _e( 'Title:', \JustCustomFields::TEXTDOMAIN ); ?></label>
+			<input class="widefat" id="<?php echo esc_attr( $this->get_field_id( 'title' ) ); ?>"
+				   name="<?php echo $this->get_field_name( 'title' ); ?>" type="text"
+				   value="<?php echo esc_attr( $title ); ?>"/>
 		</p>
-		<p><label for="<?php echo $this->get_field_id( 'show_monthes' ); ?>"><input class="checkbox"
-																					id="<?php echo $this->get_field_id( 'show_monthes' ); ?>"
-																					name="<?php echo $this->get_field_name( 'show_monthes' ); ?>"
-																					type="checkbox"
-																					value="1" <?php echo $show_monthes; ?> /> <?php _e( 'Show month/year select boxes', \JustCustomFields::TEXTDOMAIN ); ?>
+		<p><label for="<?php echo esc_attr( $this->get_field_id( 'show_monthes' ) ); ?>">
+				<input class="checkbox"
+					   id="<?php echo esc_attr( $this->get_field_id( 'show_monthes' ) ); ?>"
+					   name="<?php echo $this->get_field_name( 'show_monthes' ); ?>"
+					   type="checkbox"
+					   value="1" <?php echo esc_attr( $show_monthes ); ?> /> <?php _e( 'Show month/year select boxes', \JustCustomFields::TEXTDOMAIN ); ?>
 			</label></p>
 		<p>
-			<label for="<?php echo $this->get_field_id( 'date_format' ); ?>"><?php _e( 'Date format:', \JustCustomFields::TEXTDOMAIN ); ?></label>
-			<input class="widefat" id="<?php echo $this->get_field_id( 'date_format' ); ?>"
+			<label for="<?php echo esc_attr( $this->get_field_id( 'date_format' ) ); ?>"><?php _e( 'Date format:', \JustCustomFields::TEXTDOMAIN ); ?></label>
+			<input class="widefat" id="<?php echo esc_attr( $this->get_field_id( 'date_format' ) ); ?>"
 				   name="<?php echo $this->get_field_name( 'date_format' ); ?>" type="text"
-				   value="<?php echo $date_format; ?>"/><br/>
+				   value="<?php echo esc_attr( $date_format ); ?>"/><br/>
 			<small><?php _e( 'Example:', \JustCustomFields::TEXTDOMAIN ); ?> yy-mm-dd <a
 						href="http://api.jqueryui.com/datepicker/#option-dateFormat"
 						target="_blank"><?php _e( 'look more about date formats', \JustCustomFields::TEXTDOMAIN ); ?></a>
@@ -101,7 +103,11 @@ class JustField_DatePicker extends core\JustField {
 	}
 
 	/**
-	 *    save field on post edit form
+	 *  Save field on post edit form
+	 *
+	 * @param array $values values.
+	 *
+	 * @return array
 	 */
 	public function save( $values ) {
 		$values = $values['val'];
@@ -110,7 +116,12 @@ class JustField_DatePicker extends core\JustField {
 	}
 
 	/**
-	 *    update instance (settings) for current field
+	 * Update instance (settings) for current field
+	 *
+	 * @param array $new_instance New instance.
+	 * @param array $old_instance Old instance.
+	 *
+	 * @return array
 	 */
 	public function update( $new_instance, $old_instance ) {
 		$instance                 = $old_instance;
@@ -121,6 +132,9 @@ class JustField_DatePicker extends core\JustField {
 		return $instance;
 	}
 
+	/**
+	 * Add script for collection and custom scripts and styles from datapicker fields
+	 */
 	public function add_js() {
 		/**
 		 * WP version 3.0 and above have datepicker ui-core;
@@ -128,6 +142,9 @@ class JustField_DatePicker extends core\JustField {
 		wp_enqueue_script( 'jquery-ui-datepicker' );
 	}
 
+	/**
+	 * Add styles for collection and custom scripts and styles from datapicker fields
+	 */
 	public function add_css() {
 		wp_register_style( 'jcf_ui_datepicker', jcf_plugin_url( 'components/datepicker/ui-theme-smoothness/jquery-ui-1.8.13.custom.css' ) );
 		wp_enqueue_style( 'jcf_ui_datepicker' );
